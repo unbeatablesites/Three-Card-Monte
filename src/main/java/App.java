9874b1195c185c35bd881523384798b26ee60504
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-10 21:33:53 -0400

    Add some logic

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args) {
+        doLogic();
+    }
+
+
+    public static void doLogic(){
+
+        System.out.println("Working");
+        System.out.println("OK OK");
+
+    }
+};
