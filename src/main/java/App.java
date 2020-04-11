97614c85973c5fd7d1385e1b84fbbc9241bd3639
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-11 13:12:47 -0400

    Add some logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public class App {
     }
 
 
-    public static void displayMessage(){
+    public static void displayMessage() {
 
         System.out.println("You slide up to Fast Eddie's card table and plop down your cash.\n" +
                 "He glances at you out of the corner of his eye and starts shuffling.\n" +
@@ -21,28 +21,12 @@ public class App {
                 "\n" +
                 "\t##  ##  ##\n" +
                 "\t##  ##  ##\n" +
-                "\t1   2   3\n"+"Enter selection here  ++++++>");
+                "\t1   2   3\n" + "Enter selection here  ++++++>");
 
 
     }
 
-    public static void displayMessageForOne(){
-
-        System.out.println("    AA  ##  ##\n" +
-                "\tAA  ##  ##\n" +
-                "\t1   2   3");
-
-
-    }
-    public static void displayMessageForTwo(){
-
-        System.out.println("    AA  ##  ##\n" +
-                "\tAA  ##  ##\n" +
-                "\t1   2   3");
-
-
-    }
-    public static void displayMessageForThree(){
+    public static void displayMessageForOne() {
 
         System.out.println("    AA  ##  ##\n" +
                 "\tAA  ##  ##\n" +
@@ -51,33 +35,48 @@ public class App {
 
     }
 
+    public static void displayMessageForTwo() {
 
-    public static void selectRandomDeckOfCards(){
+        System.out.println("    AA  ##  ##\n" +
+                "\tAA  ##  ##\n" +
+                "\t1   2   3");
+
+
+    }
+
+    public static void displayMessageForThree() {
+
+        System.out.println("    AA  ##  ##\n" +
+                "\tAA  ##  ##\n" +
+                "\t1   2   3");
+
+
+    }
+
+
+    public static void selectRandomDeckOfCards() {
 
         Scanner scan = new Scanner(System.in);
         Random randomDeck = new Random();
 
-        int randomNumber = 1 +randomDeck.nextInt(3);
-        int userInput  = scan.nextInt();
+        int randomNumber = 1 + randomDeck.nextInt(3);
+        int userInput = scan.nextInt();
 
-        if(userInput == 1 && randomNumber == 1 ){
+        if (userInput == 1 && randomNumber == 1) {
             System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
             displayMessageForOne();
 
-        } else if(userInput == 2 && randomNumber == 2){
+        } else if (userInput == 2 && randomNumber == 2) {
             System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
-            System.out.println("    ##  AA  ##\n" +
-                    "\t##  AA  ##\n" +
-                    "\t1   2   3");
+            displayMessageForTwo();
 
-        }else if(userInput == 3 && randomNumber == 3){
+        } else if (userInput == 3 && randomNumber == 3) {
             System.out.println("You nailed it! Fast Eddie reluctantly hands over your winnings, scowling.");
-            System.out.println("    ##  ##  AA\n" +
-                    "\t##  ##  AA\n" +
-                    "\t1   2   3");
+            displayMessageForThree();
 
-        }else{
+        }else if (userInput == 1 && randomNumber != 1) {
             System.out.println("Ha! Fast Eddie wins again! The ace was card number "+randomNumber+".");
-            }
+            displayMessageForOne();
         }
     }
+}
